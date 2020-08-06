17f42c398aebbec8efee0a2f7d8993591344e7f9
Ilemar <ilemarandrade@gmail.com>
2020-08-06 16:39:55 -0400

    verificacion de un error completado

--- src/components/RegisterLoginLayout/Register.js
+++ src/components/RegisterLoginLayout/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TextField from '@material-ui/core/TextField';
 import {styles, useStylesComplete} from "../../theme/theme.js";
 import {ButtonDashboard} from "../../theme/theme.js";
@@ -9,35 +9,45 @@ import "../../App.css"
 function Register(){
 const classes =useStylesComplete();
 let history = useHistory();
-  const handlerRegistrar=()=>{
-    history.push("/dashboard");
-  }
 const [errorDocumento, seterrorDocumento]=useState(false);  
 const [errorNombre, seterrorNombre]=useState(false);
 const [errorEmail, seterrorEmail]=useState(false);  
 const [errorCelular, seterrorCelular]=useState(false);  
-
-
-  const handlerErrorDocumento=(e)=>{
-    seterrorDocumento(validateOnlyNumbers(e.target.value))
+  
+const handlerRegistrar=()=>{
+  if((!errorDocumento && !errorEmail) && (!errorCelular && !errorNombre)){
+    history.push("/dashboard");
+  }  
+  else{
+    alert("Completa los campos correctamente");
   }
-  const handlerErrorNombre=(e)=>{
+  
+
+}
+  const handlerDocumento=(e)=>{
+    seterrorDocumento(validateOnlyNumbers(e.target.value))
+    console.log(errorDocumento)
+  }
+  const handlerNombre=(e)=>{
     seterrorNombre(validateOnlyLetters(e.target.value))
   }
-  const handlerErrorEmail=(e)=>{
+  const handlerEmail=(e)=>{
     seterrorEmail(validateEmail(e.target.value));
+    //setusuarioRegistrado(usuarioRegistrado.email=e.target.value)  
   }
-  const handlerErrorCelular=(e)=>{
+  const handlerCelular=(e)=>{
     seterrorCelular(validateOnlyNumbers(e.target.value))
+    //setusuarioRegistrado(usuarioRegistrado.celular=e.target.value);
   }
+
     return(
         <div>
             <h2 style={{textAlign:"center",color:"white"}}>Sign up</h2>
             <div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorDocumento} onChange={handlerErrorDocumento} inputProps={{maxlength:10}} required="true"label="Documento" variant="filled"/></div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorNombre} onChange={handlerErrorNombre} inputProps={{maxlength:40}} required="true" label="Nombres" variant="filled"/></div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorEmail}required="true" onChange={handlerErrorEmail} inputProps={{maxlength:45}} label="Email" variant="filled" /></div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorCelular} onChange={handlerErrorCelular} inputProps={{maxlength:12}}required="true" label="Celular" variant="filled" /></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="documento" error={errorDocumento} onChange={handlerDocumento} inputProps={{maxlength:10}} required="true"label="Documento" variant="filled"/></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="nombres" error={errorNombre} onChange={handlerNombre} inputProps={{maxlength:40}} required="true" label="Nombres" variant="filled"/></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="email" error={errorEmail} required="true" onChange={handlerEmail} inputProps={{maxlength:45}} label="Email" variant="filled" /></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} error={errorCelular} onChange={handlerCelular} inputProps={{maxlength:12}}required="true" label="Celular" variant="filled" /></div>
                 <div className="perfectCentered"><ButtonDashboard onClick={handlerRegistrar} variant="contained" color="#1ab187">Registrar</ButtonDashboard></div>
             </div>
         </div>
