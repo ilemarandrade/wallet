4786d0b648cbc91507cc0d5475e658722a29deec
Ilemar <ilemarandrade@gmail.com>
2020-08-06 12:25:05 -0400

    Primera fase de validadores de tipo de datos ingresados a los input. Funciona debe ser refactorizado

--- src/components/RegisterLoginLayout/Register.js
+++ src/components/RegisterLoginLayout/Register.js
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import {styles, useStylesComplete} from "../../theme/theme.js";
 import {ButtonDashboard} from "../../theme/theme.js";
 import { useHistory } from "react-router-dom";
+import {validateOnlyLetters, validateOnlyNumbers, validateEmail} from "../Validadores/validadorInput.js";
 import "../../App.css"
 
 function Register(){
@@ -11,15 +12,32 @@ let history = useHistory();
   const handlerRegistrar=()=>{
     history.push("/dashboard");
   }
+const [errorDocumento, seterrorDocumento]=useState(false);  
+const [errorNombre, seterrorNombre]=useState(false);
+const [errorEmail, seterrorEmail]=useState(false);  
+const [errorCelular, seterrorCelular]=useState(false);  
 
+
+  const handlerErrorDocumento=(e)=>{
+    seterrorDocumento(validateOnlyNumbers(e.target.value))
+  }
+  const handlerErrorNombre=(e)=>{
+    seterrorNombre(validateOnlyLetters(e.target.value))
+  }
+  const handlerErrorEmail=(e)=>{
+    seterrorEmail(validateEmail(e.target.value));
+  }
+  const handlerErrorCelular=(e)=>{
+    seterrorCelular(validateOnlyNumbers(e.target.value))
+  }
     return(
         <div>
             <h2 style={{textAlign:"center",color:"white"}}>Sign up</h2>
             <div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" label="Documento" variant="filled"/></div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" label="Nombres" variant="filled"/></div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}}id="filled-basic" label="Email" variant="filled" /></div>
-                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" label="Celular" variant="filled" /></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorDocumento} onChange={handlerErrorDocumento} inputProps={{maxlength:10}} required="true"label="Documento" variant="filled"/></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorNombre} onChange={handlerErrorNombre} inputProps={{maxlength:40}} required="true" label="Nombres" variant="filled"/></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorEmail}required="true" onChange={handlerErrorEmail} inputProps={{maxlength:45}} label="Email" variant="filled" /></div>
+                <div style={styles.marginBottom}><TextField classes={{root: classes.root}} id="filled-basic" error={errorCelular} onChange={handlerErrorCelular} inputProps={{maxlength:12}}required="true" label="Celular" variant="filled" /></div>
                 <div className="perfectCentered"><ButtonDashboard onClick={handlerRegistrar} variant="contained" color="#1ab187">Registrar</ButtonDashboard></div>
             </div>
         </div>
---
+++ src/components/Validadores/validadorInput.js
@@ -0,0 +1,18 @@
+
+const regexEmail=/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
+
+export function validateOnlyNumbers(value) {
+  if(!value.match(/[\WA-Za-z]/g)===false){
+    return true;
+  } 
+}
+
+export function validateOnlyLetters(value){
+  if(/[\sA-Za-z]/.test(value)===false) {
+    return true
+  }
+}
+export function validateEmail(value){
+ console.log(regexEmail.test(value))
+ return regexEmail.test(value) ? false: true; 
+}
