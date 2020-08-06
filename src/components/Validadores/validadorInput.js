435faec7b5cc84fdfb80c854590462c9ebb9d3f4
Ilemar <ilemarandrade@gmail.com>
2020-08-06 14:11:48 -0400

    listo bug con la validacion de los nombres

--- src/components/Validadores/validadorInput.js
+++ src/components/Validadores/validadorInput.js
@@ -1,6 +1,6 @@
 
 const regexEmail=/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
-
+const nombres=/^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/
 export function validateOnlyNumbers(value) {
   if(!value.match(/[\WA-Za-z]/g)===false){
     return true;
@@ -8,11 +8,10 @@ export function validateOnlyNumbers(value) {
 }
 
 export function validateOnlyLetters(value){
-  if(!value.match(/[\W0-9]/g)===false) {
+  if(!value.match(nombres)) {
     return true
   }
 }
 export function validateEmail(value){
- console.log(regexEmail.test(value))
  return regexEmail.test(value) ? false: true; 
 }
