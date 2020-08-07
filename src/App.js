a632e98f34e71c97db1143a27747542fe89e3992
Ilemar <ilemarandrade@gmail.com>
2020-08-07 10:42:13 -0400

    Se coloque en todas la vistas del usuario un PrivateRoute para que verifique la aplicacion si hay alguien logueado

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Dashboard from "./components/Dashboard.js" ;
 import Consulta from "./components/Consulta.js"
 import Recargar from "./components/Recargar.js"
 import Pagar from "./components/Pagar.js"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   useEffect(() => {crearStorage();},[]);
@@ -21,18 +22,18 @@ function App() {
             <Route exact path="/">
               <RegisterOrLogin></RegisterOrLogin>
             </Route>
-            <Route exact  path="/dashboard">
+            <PrivateRoute exact  path="/dashboard">
               <Dashboard></Dashboard>
-            </Route>
-            <Route exact  path="/dashboard/recarga">
+            </PrivateRoute>
+            <PrivateRoute exact  path="/dashboard/recarga">
               <Recargar></Recargar>
-            </Route>
-            <Route exact  path="/dashboard/pagar">
+            </PrivateRoute>
+            <PrivateRoute exact  path="/dashboard/pagar">
               <Pagar></Pagar>
-            </Route>
-            <Route exact  path="/dashboard/consultadesaldo">
+            </PrivateRoute>
+            <PrivateRoute exact  path="/dashboard/consultadesaldo">
               <Consulta></Consulta>
-            </Route>
+            </PrivateRoute>
           </Switch>
           </div> 
       </Router>
