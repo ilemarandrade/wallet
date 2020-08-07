09ba7ff99b2cbde319e98461861cd329cef9afa9
Ilemar <ilemarandrade@gmail.com>
2020-08-07 12:28:45 -0400

    Se agrego un div que diga el nombre del usuario logueado,

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,14 +3,25 @@ import { Route, useHistory } from 'react-router-dom'
 
 const PrivateRoute = props => {
     const [isLoading, setIsLoading] = React.useState(true)
+    const [usuario, setUsuario]=React.useState("")
     const history = useHistory()
     React.useEffect(() => {
-        JSON.parse(localStorage.getItem("userLogin"))===null ? 
-        setTimeout(() => {history.push('/')}, 5000):setIsLoading(false)         
+        if(JSON.parse(localStorage.getItem("userLogin"))===null) { 
+        setTimeout(() => {history.push('/')}, 5000)
+        }
+        else {
+            setIsLoading(false)
+            setUsuario(JSON.parse(localStorage.getItem("userLogin"))[0]["userOnline"])
+        }         
+           
 
 
 
     },[history])
+    const cerrarSesion=()=>{
+        history.push('/')
+        localStorage.removeItem('userLogin');
+    }
 
     return (
     
@@ -18,11 +29,24 @@ const PrivateRoute = props => {
           {isLoading ? (
             <div style={{color:"white"}}>Cargando...</div>
           ) : (
-            <Route {...props}>{props.children}</Route>
+          <Route {...props}><div style={stylesOfuserActived}>User: {usuario} <br></br><p onClick={cerrarSesion} style={pCerrarSesion}>CerrarSesion</p></div>{props.children}</Route>
           )}
         </>
       )
 }
 
+const stylesOfuserActived={
+    background:"#1ab187",
+    color:"white",
+    width: "200px",
+    textAlign: "center",
+    padding: "1px 0",
+    borderRadius: "5px 70px"
+}
+const pCerrarSesion={
+    textDecoration:"underline",
+    color:"#146f56",
+    margin:"0px"
+}
 
 export default PrivateRoute
