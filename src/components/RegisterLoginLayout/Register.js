7f51b87ee043d3197083fb2a9982bc531d854494
Ilemar <ilemarandrade@gmail.com>
2020-08-06 20:03:21 -0400

    Ya arregle bug falta refactorizar y coloque useeffect cuando se crea el localStorage

--- src/components/RegisterLoginLayout/Register.js
+++ src/components/RegisterLoginLayout/Register.js
@@ -11,16 +11,21 @@ function Register(){
 const classes =useStylesComplete();
 const valueSignUp={"documento":"","nombres":"","email":"","celular":"","saldo":"1000"}
 let history = useHistory();
+
 const [errorDocumento, seterrorDocumento]=useState(false);  
 const [errorNombre, seterrorNombre]=useState(false);
 const [errorEmail, seterrorEmail]=useState(false);  
 const [errorCelular, seterrorCelular]=useState(false); 
 const [signUp,setSignUp]=useState(valueSignUp);
-const [started, setStarted]=useState(false);
+const [valueDocumento,setvalueDocumento]=useState("");
+const [valueNombre,setvalueNombre]=useState("");
+const [valueEmail,setvalueEmail]=useState("");
+const [valueCelular,setvalueCelular]=useState("");
+
 
   
 const handlerRegistrar=()=>{
-  if(started===false){
+  if(valueDocumento==="" || valueNombre===""  || valueEmail==="" || valueCelular===""){
     return
   }
   if((!errorDocumento && !errorEmail) && (!errorCelular && !errorNombre)){
@@ -32,20 +37,23 @@ const handlerRegistrar=()=>{
   }
 }
   const handlerDocumento=(e)=>{
-    setStarted(true);
     seterrorDocumento(validateOnlyNumbers(e.target.value))
+    setvalueDocumento(e.target.value);
     setSignUp({...signUp, documento:e.target.value});
   }
   const handlerNombre=(e)=>{
     seterrorNombre(validateOnlyLetters(e.target.value))
+    setvalueNombre(e.target.value);
     setSignUp({...signUp, nombres:e.target.value});
   }
   const handlerEmail=(e)=>{
     seterrorEmail(validateEmail(e.target.value));
+    setvalueEmail(e.target.value);
     setSignUp({...signUp, email:e.target.value});
   }
   const handlerCelular=(e)=>{
     seterrorCelular(validateOnlyNumbers(e.target.value))
+    setvalueCelular(e.target.value);
     setSignUp({...signUp, celular:e.target.value});;
   }
 
--- src/components/RegisterOrLogin.js
+++ src/components/RegisterOrLogin.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import "./RegisterOrLogin.css";
 import {crearStorage} from "../localStorage.js"
 import Register from "./RegisterLoginLayout/Register.js"
@@ -11,7 +11,7 @@ function RegisterOrLogin(){
         e.target.id==="Signup" && setchangeProcess(true)
         e.target.id!=="Signup" && setchangeProcess(false)
     }
-    crearStorage();
+    useEffect(() => {crearStorage();},[]);
     
 return(
     <div id="registerOrLogin"className="perfectCentered">
