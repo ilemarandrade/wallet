e4276a4e09a10016e20e331ca1fe41b600b99a7a
Ilemar <ilemarandrade@gmail.com>
2020-08-07 13:16:00 -0400

    Agregue boton para volver a dashboard

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Route, useHistory } from 'react-router-dom'
 
 const PrivateRoute = props => {
-    const [isLoading, setIsLoading] = React.useState(true)
-    const [usuario, setUsuario]=React.useState("")
-    const history = useHistory()
+    const [isLoading, setIsLoading] =useState(true)
+    const [usuario, setUsuario]=useState("")
+    const [notDashboard, setnotDashboard]=useState(false);
+    const history = useHistory();
+    console.log(history.location.pathname)
     React.useEffect(() => {
+        
+        history.location.pathname === "/dashboard" ? setnotDashboard(false): setnotDashboard(true);
+        console.log("entra a use effect")
         if(JSON.parse(localStorage.getItem("userLogin"))===null) { 
         setTimeout(() => {history.push('/')}, 5000)
         }
+        
         else {
             setIsLoading(false)
             setUsuario(JSON.parse(localStorage.getItem("userLogin"))[0]["userOnline"])
@@ -17,10 +23,13 @@ const PrivateRoute = props => {
 
 
 
-    },[history])
+    },[history.location.pathname])
     const cerrarSesion=()=>{
         history.push('/')
         localStorage.removeItem('userLogin');
+    }
+    const backToDashboard=()=>{
+        history.push('/dashboard')
     }
 
     return (
@@ -29,7 +38,16 @@ const PrivateRoute = props => {
           {isLoading ? (
             <div style={{color:"white"}}>Cargando...</div>
           ) : (
-          <Route {...props}><div style={stylesOfuserActived}>User: {usuario} <br></br><p onClick={cerrarSesion} style={pCerrarSesion}>CerrarSesion</p></div>{props.children}</Route>
+          <Route {...props}
+            >
+            <div style={{display:"flex"}}>
+              <div style={stylesOfuserActived}>User: {usuario} <br></br>
+              <p onClick={cerrarSesion} style={pCerrarSesion}>CerrarSesion</p></div>
+              <div onClick={backToDashboard} style={{...stylesOfBackDashboard, display: notDashboard ? "":"none",}}>
+              <p  onClick={backToDashboard}>Volver a Dashboard</p></div>
+            </div>
+              {props.children}
+          </Route>
           )}
         </>
       )
@@ -42,6 +60,16 @@ const stylesOfuserActived={
     textAlign: "center",
     padding: "1px 0",
     borderRadius: "5px 70px"
+}
+const stylesOfBackDashboard={
+    background:"#1ab187",
+    color:"white",
+    width: "200px",
+    textAlign: "center",
+    padding: "1px 0",
+    borderRadius: "5px 70px",
+    marginTop:"1px",
+
 }
 const pCerrarSesion={
     textDecoration:"underline",
