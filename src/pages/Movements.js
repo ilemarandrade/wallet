7fb8e029e74838bbe0ba7d68fe2405d9f4a9c188
Ilemar <ilemarandrade@gmail.com>
2023-02-08 14:16:59 -0400

    feat: improve of movements view

--- src/pages/Movements.js
+++ src/pages/Movements.js
@@ -4,18 +4,31 @@ import useMovements from "../hook/api/useMovements";
 import TableMovements from "../components/TableMovements";
 import { toast } from "react-hot-toast";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import routes from "../constants/routes";
 
 function Pay() {
+  const history = useHistory();
   const { t } = useTranslation();
   const { data } = useMovements({
+    onSuccess: ({ movements }) => {
+      if (!movements.length) {
+        toast.error(`${t("toast_message.whithout_movements")}`);
+        history.push(routes.DASHBOARD);
+      }
+    },
     onError: () => {
       toast.error(`${t("toast_message.there_is_error")}`);
     },
   });
   return (
-    <MainLayout title={t("services.account_movements")}>
-      {data && <TableMovements data={data?.movements} />}
-    </MainLayout>
+    <>
+      {data?.movements.length ? (
+        <MainLayout title={t("services.account_movements")}>
+          <TableMovements data={data?.movements} />
+        </MainLayout>
+      ) : null}
+    </>
   );
 }
 
