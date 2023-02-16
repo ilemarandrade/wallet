746e3564494ad57098fd25733dc13c8c52454b07
Ilemar <ilemarandrade@gmail.com>
2023-02-16 14:43:30 -0400

    feat: improve of Movements view

--- src/pages/Movements.js
+++ src/pages/Movements.js
@@ -6,7 +6,21 @@ import { toast } from "react-hot-toast";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import routes from "../constants/routes";
+import styled from "styled-components";
 
+const MainLayoutStyles = styled(MainLayout)(
+  ({ theme }) => `
+
+  ${[theme.breakpoints.down(600)]}{
+    & > div {
+      min-width: 100vw;
+      padding: 0px;
+      margin-top: 1.5rem;
+    }
+  }
+
+`
+);
 function Pay() {
   const history = useHistory();
   const { t } = useTranslation();
@@ -24,9 +38,9 @@ function Pay() {
   return (
     <>
       {data?.movements.length ? (
-        <MainLayout title={t("services.account_movements")}>
-          <TableMovements data={data?.movements} />
-        </MainLayout>
+        <MainLayoutStyles title={t("services.account_movements")}>
+          <TableMovements data={data.movements} />
+        </MainLayoutStyles>
       ) : null}
     </>
   );
